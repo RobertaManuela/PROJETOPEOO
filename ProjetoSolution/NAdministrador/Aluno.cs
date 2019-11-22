826989cdedbc6b8cb78d6cbd306c19a349668b41
Roberta Manuela <robertamanuellyaraujo@hotmail.com>
2019-11-22 09:52:12 -0300

    CRUD de uma identidade

---
+++ ProjetoSolution/NAdministrador/Aluno.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NAdministrador
+{
+    public class Aluno
+    {
+        private List<Aluno> a = new List<Aluno>();
+        private PAluno i;
+
+        public void InserirAluno(Aluno x)
+        {
+            a.Add(x);
+        }
+
+        public List<Aluno> ListarAluno()
+        {
+            i = new PAluno();
+            return i.Open().a.ToList();
+        }
+
+        public void AtualizarAluno(Aluno x)
+        {
+            i = new PAluno();
+            List<PAluno> att = i.Open();
+
+            for( int j = 0;  j < att.Count; j++)
+            {
+                if(att[j].GetAlunoId() == x.GetAlunoId())
+                {
+                    att.RemoveAt(j);
+                }
+            }
+            att.Add(x);
+            i.Save(att);
+        }
+
+        public void ExcluirAluno(Aluno x)
+        {
+            i = new PAluno();
+            List<Aluno> excluir = i.Open();
+
+            for(int j = 0; j < excluir.Count; j++)
+            {
+                if(excluir[i].AlunoId == x.AlunoId())
+                {
+                    excluir.RemoveAt(j);
+                }
+            }
+            i.Save(excluir);
+        }
+    }
+}
