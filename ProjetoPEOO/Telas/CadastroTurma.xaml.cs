88367a9b98e10c17896d4369e05b42728f9b1a48
Roberta Manuela <robertamanuellyaraujo@hotmail.com>
2019-12-03 11:08:14 -0300

    algumas atts

--- ProjetoPEOO/Telas/CadastroTurma.xaml.cs
+++ ProjetoPEOO/Telas/CadastroTurma.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Modelo;
+using Negócio;
 
 namespace Telas
 {
@@ -19,10 +21,58 @@ namespace Telas
     /// </summary>
     public partial class CadastroTurma : Window
     {
+        NTurma n = new NTurma();
         public CadastroTurma()
         {
             InitializeComponent();
-            Cb.ItemsSource = 
+            Cb.ItemsSource = n.ListarDisciplinas();
+            Bc.ItemsSource = n.ListarTurma();
+        }
+
+        private void InsertClick(object sender, RoutedEventArgs e)
+        {
+            MTurma x = new MTurma();
+            x.Semestre = txtSemestre.Text;
+            Cb.SelectedItem
+            n.InserirTurma(x);
+        }
+
+        private void SelectClick(object sender, RoutedEventArgs e)
+        {
+            grid.ItemsSource = n.ListarTurma();
+        }
+
+        private void UpdateClick(object sender, RoutedEventArgs e)
+        {
+            MTurma x = new MTurma();
+            x.Semestre = txtSemestre.Text;
+            n.AtualizarTurma(x);
+            grid.ItemsSource = null;
+            grid.ItemsSource = n.ListarTurma();
+        }
+
+        private void DeleteClick(object sender, RoutedEventArgs e)
+        {
+            if (grid.SelectedItem != null)
+            {
+                MTurma a = grid.SelectedItem as MTurma;
+                n.ExcluirTurma(a);
+                grid.ItemsSource = null;
+                grid.ItemsSource = n.ListarTurma();
+            }
+        }
+
+        private void grid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (grid.SelectedItem != null)
+            {
+                MTurma c = grid.SelectedItem as MTurma;
+                x.Semestre = txtSemestre.Text;
+            }
+            else
+            {
+                txtSemestre.Text = null;
+            }
         }
     }
 }
